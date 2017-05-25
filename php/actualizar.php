445181400ada00180f5458ed0604620001f9ea05
Dangor05 <dani.ramos92@hotmail.com>
2017-05-25 01:08:06 -0600

    uno mas

--- php/actualizar.php
+++ php/actualizar.php
@@ -19,12 +19,46 @@ if(!empty($_POST) && !empty($_FILES)){
 		$sv02std=mysqli_real_escape_string($con,$_POST['sv02std']);
 		$sv04plan=$_FILES['sv04plan'];
 		$path="../archivos/".$sv03cedp."\ ";
+		$sv09fumv=date("Y-m-d");
+
+		echo $sv09nfol; echo " "; echo "1" ;
+		echo "<br>";
+		echo $sv09npre;echo " "; echo "2" ;
+		echo "<br>";
+		echo $sv09mnt;echo " "; echo  "3";
+		echo "<br>";
+		echo $sv09fvdp;echo " "; echo "4" ;
+		echo "<br>";
+		echo $sv09fumv;echo " "; echo "5" ;
+		echo "<br>";
+		echo $sv08conse;echo " "; echo "6" ;
+		echo "<br>";
+		echo $sv01cedc;echo " ";echo "7" ;
+		echo "<br>";
+		echo $sv03cedp; echo " ";echo "8" ;
+		echo "<br>";
+		echo $sv04nfin;echo " "; echo "9" ;
+		echo "<br>";
+		echo $sv02code;echo " "; echo "10" ;
+		echo "<br>";
+		echo $sv07cdtp;echo " "; echo "11" ;
+		echo "<br>";
+		echo $sv09npln;echo " "; echo "12" ;
+		echo "<br>";
+
+
 
 		
 
-		$stm="UPDATE sv09vsdo set sv09nfol='$sv09nfol', sv09npre='$sv09npre', sv09mnt='$sv09mnt', sv09fvdp='$sv09fvdp', sv09fumv=NOW(), sv08conse='$sv08conse', sv03cedp='$sv03cedp', sv04nfin='$sv04nfin', sv02code='$sv02code', sv07cdtp='$sv07cdtp' WHERE sv09npln='$sv09npln'";
-		
+		$stm="UPDATE sv09vsdo set sv09nfol='$sv09nfol', sv09npre='$sv09npre', sv09mnt='$sv09mnt', sv09fvdp='$sv09fvdp', sv09fumv=NOW(), sv08conse='$sv08conse',sv01cedc='$sv01cedc', sv03cedp='$sv03cedp', sv04nfin='$sv04nfin', sv02code='$sv02code', sv07cdtp='$sv07cdtp' WHERE sv09npln='$sv09npln'";
 		$sql="UPDATE sv08trmte SET sv02code='$sv02std' WHERE sv08conse='$sv08conse'";
+
+		//$stm=$con->prepare("UPDATE sv09vsdo SET `sv09nfol`=?, `sv09npre`=?,`sv09mnt`=?, `sv09fvdp`=?, `sv09fumv`=?, `sv08conse`='?', `sv01cedc`=?, `sv03cedp`=?, `sv04nfin`=?, `sv02code`=?, `sv07cdtp`=? WHERE `sv09npln`=?");
+		//$stm->bind_param("sssssssssiss",$sv09nfol,$sv09npre,$sv09mnt,$sv09fvdp,$sv09fumv,$sv08conse,$sv01cedc,$sv03cedp,$sv04nfin,$sv02code,$sv07cdtp,$sv09npln);
+
+
+		
+		
 
 		if (!empty($sv04plan['name'])) {
 			$st ="UPDATE sv04reqtos SET sv04apln='".$sv04plan['name']."' WHERE sv04nfin='$sv04nfin'";
