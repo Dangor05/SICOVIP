91e098c0d8a8eae00869d687aceb9b29cf506eba
Dangor05 <dani.ramos92@hotmail.com>
2017-10-06 20:57:37 -0600

    Prueba

--- php/lib/conf.php
+++ php/lib/conf.php
@@ -6,8 +6,6 @@ define ('DB_PASS','');
 define ('DB_NAME','sicovip');
 
 
-define('MAIL','danielramosr45@gmail.com');
-define('PASS','juanpaulo');
 
 //define('MAIL','catastopo.munisantacruz@gmail.com');
 //define('PASS','VISA99topo');
